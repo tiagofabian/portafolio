7929c56dd3f43be018bdf4de3d6817af4700e2f3
tiago <tiagofabian195@outlook.com>
2024-07-10 16:46:44 -0400

    new gitproject extra-file

--- src/pages/Extra.js
+++ src/pages/Extra.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import "../assets/styles/layout.css";
-import Soon from "../components/extra/Soon";
+// import Soon from "../components/extra/Soon";
+import GitprojectPreview from '../components/extra/GitprojectPreview';
 
 const Extra = () => {
   return (
     <div className='layout-container'>
-      <Soon/>
+      <GitprojectPreview />
     </div>
   )
 }
