774a3b1c7828abc08d1802dc882027a2bf90ad73
tiago <tiagofabian195@outlook.com>
2024-09-05 15:40:54 -0400

    about-description update

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -13,15 +13,14 @@ const Nav = () => {
                     <h1><span>TIAGO</span> <br /> <span id="cyan">ALCÁZAR</span></h1>
                     <h4>DESAROLLADOR DE SOFTWARE</h4>
                     <p className='aboutme-description'>
-                        Soy un desarrollador fullstack
-                        especializado en JavaScript, con
-                        experiencia en el manejo de proyectos
-                        a gran escala, incluyendo plataformas
-                        de E-commerce y otras aplicaciones
-                        web. A lo largo de mi carrera
-                        he estudiado diferentes tipos de
-                        tecnologías y siempre estoy listo para
-                        afrontar nuevos desafios.
+                        Soy un desarrollador fullstack especializado 
+                        en React con Node. Poseo experiencia diseñando 
+                        aplicaciones web con multiples librerias y 
+                        herramientas de optiomización, prueba, 
+                        documentación y despliegue. A lo largo de mi 
+                        carrera, he estudiado diferentes tipos de 
+                        tecnologías y siempre estoy listo para afrontar 
+                        nuevos desafíos.
                     </p>
                 </div>
             </div>
