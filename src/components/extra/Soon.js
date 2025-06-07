1aa0d0936e2752591143035dcf78f9cefd1632c5
tiago <tiagofabian195@outlook.com>
2025-06-07 16:50:24 -0400

    resolved conflict 2

---
+++ src/components/extra/Soon.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "../../assets/styles/extra/soon.css";
+import { ReactComponent as ReactIcon } from './../../assets/img/svg/icon-react-purp512.svg';
+
+const Soon = () => {
+  return (
+    <div style={{display: "flex", alignItems: "center"}}>
+      &nbsp; <span>...Proximamente</span> &nbsp;
+      <ReactIcon width="22" height="22" fill="black"/>
+    </div>
+  )
+}
+
+export default Soon;
