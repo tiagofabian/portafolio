d265456914971bc8be70d7e3a7cc13c4a402657e
Tiago Alcázar <tiagofabian195@outlook.com>
2026-01-27 14:30:09 -0300

    min-h-screen section home

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -17,7 +17,7 @@ export default function DashboardLayout({
     <div className="dashboard relative w-full h-full overflow-hidden px-6xs py-6xs mb:px-0 mb:py-0">
       <BubbleAnimation className="w-full h-full"/>
       <Header className="relative flex-row border border-[#ababab] gap-20xl pt-3xl pb-2xs px-5xl rounded-md aspect-[3.34/1] bg-cover bg-blue-desk shadow-box-1 tb:px-10xl tb:pt-8xl tb:pb-sm mb:flex-col mb:justify-normal mb:border-none mb:gap-10xl mb:px-8xl mb:pt-64xl mb:pb-5xl tb:rounded-s mb:rounded-none mb:shadow-box-2 mb:aspect-[4/3] mb:bg-blue-mob"/>
-      <section className="relative flex-col py-sm px-5xl tb:px-10xl tb:py-md mb:px-2xl mb:py-2xl">
+      <section className="relative flex-col py-sm px-5xl tb:px-10xl tb:py-md mb:px-2xl mb:py-2xl min-h-screen">
         {children}
       </section>
       <Footer className="relative flex-col pt-2xs pb-6xs px-md gap-4xs rounded-md shadow-box-1 tb:px-4xl tb:pt-sm tb:pb-5xs mb:px-5xl mb:pt-5xl mb:pb-4xs tb:rounded-s mb:rounded-none mb:shadow-box-2 mb:gap-md" />
