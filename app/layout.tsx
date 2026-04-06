0a5e7662edcb521e6c44d6170accd1e8e1a30ba5
Tiago Alcázar <tiagofabian195@outlook.com>
2026-04-06 18:56:37 -0400

    speed insights added

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,14 +3,21 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ToasterProvider } from "@/lib/providers/ToasterProvider";
 import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const inter = Inter({ 
   subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-  title: "portafolio",
-  description: "next v15.3",
+  metadataBase: new URL('https://portafolio-w8dj.vercel.app/'), 
+  title: "Portafolio",
+  description: "Portafolio de desarrollo web con React y Next.js",
+  openGraph: {
+    title: "Tiago | Portafolio",
+    description: "Portafolio profesional de desarrollo web",
+    images: ["/file.svg"],
+  },
 };
 
 export default function RootLayout({
@@ -19,11 +26,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="es">
+      <head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </head>
       <body className={inter.className}>
-        <ToasterProvider />
         {children}
+        <ToasterProvider />
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
